42413f37d483485590610c118dd814e9bab1c6d0
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-08-02 17:48:38 +0300

    Promo desktop ver.

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -5,7 +5,7 @@ import React from "react";
 function Promo(props) {
     return(
         <div className="promo">
-            <h1 className="promo__title">Учебный проект студента&nbsp;факультета Веб-разработки.</h1>
+            <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
         </div>
     )
 }
