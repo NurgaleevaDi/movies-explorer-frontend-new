593dfa8ebb8c7054a76b32d8a41734d1341f3304
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-08-02 11:00:57 +0300

    Header 768, 1280

---
+++ src/components/Main/Promo/Promo.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+function Promo(props) {
+    return(
+        // <header className="header">
+        //     <img className="header__logo" src={logo} alt="Логотип" />
+        //     <div className="header__menu">
+        //         <Link to={props.link} className="link header__link">
+        //             {props.nameLink}
+        //         </Link>
+        //         <button className="button header__button">
+        //             {props.name}
+        //         </button>
+        //     </div>
+        // </header>
+    )
+}
+export default Promo;
