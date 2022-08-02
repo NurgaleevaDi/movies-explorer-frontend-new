ce702918f15b3d87cd13a8d9f61fe9c068f38c36
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-08-02 10:15:26 +0300

    Header, fonts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,31 +7,39 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
+import Header from '../Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Main />
-        </Route>
-        <Route path="/movies">
-          <Movies />
-        </Route>
-        <Route path="/saved-movies">
-          <SavedMovies />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/signup">
-          <Register />
-        </Route>
-        <Route path="/signin">
-          <Login />
-        </Route>
-      </Switch>
-    </div>
+    <body> 
+      <div className="page"> 
+        <Switch>
+          <Route exact path="/">
+            <Header
+              link="/signin"
+              name="Войти"
+              nameLink="Регистрация"
+            />
+            <Main />
+          </Route>
+          <Route path="/movies">
+            <Movies />
+          </Route>
+          <Route path="/saved-movies">
+            <SavedMovies />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/signup">
+            <Register />
+          </Route>
+          <Route path="/signin">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </body>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "../../images/logo.svg";
+import { Link } from "react-router-dom";
+
+function Header(props) {
+    return(
+        <header className="header">
+            <img className="header__logo" src={logo} alt="Логотип" />
+            <div className="header__menu">
+                <Link to={props.link} className="header__link">
+                    {props.nameLink}
+                </Link>
+                <button className="header__button">
+                    {props.name}
+                </button>
+            </div>
+        </header>
+    )
+}
+export default Header;
