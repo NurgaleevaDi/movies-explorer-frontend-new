4f375930be9d271122c368f2d0af0d269fd851c0
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-08-14 17:08:17 +0300

    Register with form mobile-desktop ver.

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -12,9 +12,44 @@ function Register() {
                 <p className="register__title">Добро пожаловать!</p>
             </div>
             <form className="register__form">
-                <input className="register-input__name"></input>
-                <input className="register-input__email"></input>
-                <input className="register-input__password"></input>
+                <div className="register__input-container">
+                    <p className="register__input-disription">Имя</p>
+                    <input 
+                        className="register-input__text"
+                        type="text" 
+                        name="username"
+                        placeholder="Имя"
+                        value="Виталий"
+                        required
+                        minLength="2"
+                        maxLength="30"
+                    />
+                </div>
+                <div className="register__input-container">
+                    <p className="register__input-disription">E-mail</p>
+                    <input 
+                        className="register-input__text"
+                        type="text" 
+                        name="useremail"
+                        placeholder="E-mail"
+                        value="pochta@yandex.ru"
+                        required
+                        minLength="2"
+                        maxLength="30"
+                    />
+                </div>
+                <div className="register__input-container">
+                    <p className="register__input-disription">Пароль</p>
+                    <input 
+                        className="register-input__text"
+                        type="password" 
+                        name="password"
+                        placeholder="Пароль"
+                        value="Пароль"
+                        required
+                    />
+                    <span className="register__error-message">Что-то пошло не так...</span>
+                </div>  
             </form>
             <div className="register__footer">
                 <button type="button" className="register__btn">Зарегистрироваться</button>
